ac4e4770a72f225e82ea684b14dc29c6367bdb84
sanjay <sanjaybalak@gmail.com>
2015-10-08 15:40:41 +0530

    added ci

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("New change made");
 console.log("this is to test close issue by commit");
 
 app.get('/sb', function (req, res) {
-	res.status(200).json({status:'suceess',message:'hello world'});
+	res.status(200).json({status:'suceess',message:'hello world of express i am from ci'});
 	console.log({success:'true'});
 });
 app.get('/test', function (req, res) {
