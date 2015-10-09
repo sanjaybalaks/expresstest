7bb45954c0e19c678fed5200766449e613596d9e
sanjay <sanjaybalak@gmail.com>
2015-10-09 17:48:05 +0530

    modifed appsepc to add express folder

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("New change made");
 console.log("this is to test close issue by commit");
 
 app.get('/sb', function (req, res) {
-	res.status(200).json({status:'suceess',message:'test 2 from codeship'});
+	res.status(200).json({status:'suceess',message:'code deploy test 1'});
 	console.log({success:'true'});
 });
 app.get('/test', function (req, res) {
