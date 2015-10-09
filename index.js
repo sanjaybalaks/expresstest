a316348e3c92d182e8fafdb4ec11d7028b597625
sanjay <sanjaybalak@gmail.com>
2015-10-09 11:26:47 +0530

    added test 2 for codeship

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("New change made");
 console.log("this is to test close issue by commit");
 
 app.get('/sb', function (req, res) {
-	res.status(200).json({status:'suceess',message:'hello world of express i am from ci codeship and i successfully did a ci.yipee'});
+	res.status(200).json({status:'suceess',message:'test 2 from codeship'});
 	console.log({success:'true'});
 });
 app.get('/test', function (req, res) {
