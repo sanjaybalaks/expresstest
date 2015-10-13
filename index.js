513cee25640128011bcf175ce1a8074ed312f0e7
sanjay <sanjaybalak@gmail.com>
2015-10-13 17:07:49 +0530

    added env var

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var app = express();
 var port = process.env.PORT || 3000;
 console.log("New change made");
 console.log("this is to test close issue by commit");
+var environment=process.env.NODE_ENV || 'nothing';
+
 
 app.get('/ss', function (req, res) {
-	res.status(200).json({status:'suceess',message:'welcome to express ss  '});
+	res.status(200).json({status:'suceess',message:'welcome to express '+ environment});
 	console.log({success:'true'});
 });
 app.get('/sb', function (req, res) {
@@ -17,8 +19,7 @@ app.get('/test', function (req, res) {
   console.log({success:'true'});
 });
 
-var server = app.listen(port, function () {
-
+  var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
